e4c8caba16a4ff437ce81fbbe643d349e4971d51
Alessandro198502 <alessandrokcond@yahoo.com.br>
2026-07-17 15:51:48 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,12 @@
 console.log("App carregado");
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <div style={{ padding: '20px', color: 'black' }}>
+      <h1>Olá, Fisio Aline!</h1>
+      <p>O app está funcionando!</p>
+    </div>
+  </React.StrictMode>,
+)
